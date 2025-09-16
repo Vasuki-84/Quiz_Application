967cb4df9dd1fdd0d880ed243ae425f3e7d91b3d
Vasuki t <iamvasukithangaraj@gmail.com>
2025-09-16 13:17:19 +0530

    added 5 quiz questions

---
+++ script.js
@@ -0,0 +1,61 @@
+
+// Questions
+
+const questions = [
+    {
+        q: "Which keyword declares a constant?" ,
+        options: [ "var" , "let" , "const" , "function"],
+        answer: "const"
+    },
+    {         
+    q: "Which method is used to print messages in the console in JavaScript?",         
+    options: [ "print()", "console.print()", "console.log()", "log.console()" ],         
+    answer: "console.log()"     
+},     
+{         
+    q: "Which symbol is used for single-line comments in JavaScript?",         
+    options: [ "#", "//", "/*", "<!--" ],         
+    answer: "//"     
+},     
+{         
+    q: "Which company developed JavaScript?",         
+    options: [ "Microsoft", "Netscape", "Google", "Oracle" ],         
+    answer: "Netscape"     
+},     
+{         
+    q: "What is the default value of an uninitialized variable in JavaScript?",         
+    options: [ "null", "undefined", "0", "empty string" ],         
+    answer: "undefined"     
+},     
+{         
+    q: "Which method is used to combine two or more arrays in JavaScript?",         
+    options: [ "append()", "merge()", "concat()", "combine()" ],         
+    answer: "concat()"     
+}
+
+]
+
+// Needed variables :
+let index = 0 ;
+let score = 0 ;
+let username = "" ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
